c76a2bbe51cefa4038ee907545d3679dde1dbac4
kjcdev <kjciastek@gmail.com>
2021-12-29 04:08:39 +0100

    Added a blueprint to main.py

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+from routes import routes
+
 app = Flask(__name__)
+
+app.register_blueprint(routes)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
