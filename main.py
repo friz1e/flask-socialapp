089a66d2d4be44068dbcf1614e52ac5f8512934e
kjcdev <kjciastek@gmail.com>
2021-12-30 09:36:12 +0100

    Added secret key

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ app.register_blueprint(routes)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = 'super secret key'
 db = SQLAlchemy(app)
 
 if __name__ == "__main__":
