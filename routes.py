14dc7813e67f15bb6620a4fa5d87655e73cfad65
kjcdev <kjciastek@gmail.com>
2021-12-30 09:37:11 +0100

    Added an IF clause at the beggining of every route function to check if user is logged already

--- routes.py
+++ routes.py
@@ -1,18 +1,27 @@
-from flask import Blueprint, render_template, url_for, request, redirect
+from flask import Blueprint, render_template, url_for, request, redirect, session
 
 routes = Blueprint('routes', __name__, template_folder='templates')
 
 @routes.route('/loginPage')
 def loginPage():
-    return render_template('loginPage.html')
+    if session.get('email') == None:
+        return render_template('loginPage.html')
+    else:
+        return redirect(url_for('routes.mainPage'))
 
 @routes.route('/registerPage')
 def registerPage():
-    return render_template('registerPage.html')
+    if session.get('email') == None:
+        return render_template('registerPage.html')
+    else:
+        return redirect(url_for('routes.mainPage'))
 
 @routes.route('/mainPage')
 def mainPage():
-    return render_template('mainPage.html')
+    if session.get('email') != None:
+        return render_template('mainPage.html')
+    else:
+        return redirect(url_for('routes.loginPage'))
 
 def containsNumber(value):
     for character in value:
@@ -22,44 +31,55 @@ def containsNumber(value):
 
 @routes.route('/registerUser', methods=['POST', 'GET'])
 def registerUser():
-    if request.method == 'POST':
-        name = request.form['name']
-        surname = request.form['surname']
-        email = request.form['email']
-        password = request.form['password']
+    if session.get('email') == None:
+        if request.method == 'POST':
+            name = request.form['name']
+            surname = request.form['surname']
+            email = request.form['email']
+            password = request.form['password']
 
-        import re
+            import re
 
-        if not re.match(r"[^@]+@[^@]+\.[^@]+", email):
-            return render_template('registerPage.html', error="Given email doesnt contain required elements!")
+            if not re.match(r"[^@]+@[^@]+\.[^@]+", email):
+                return render_template('registerPage.html', error="Given email doesnt contain required elements!")
 
-        if (containsNumber(name) is True) or (containsNumber(surname) is True):
-            return render_template('registerPage.html', error="Given name or surname has digits!")
+            if (containsNumber(name) is True) or (containsNumber(surname) is True):
+                return render_template('registerPage.html', error="Given name or surname has digits!")
 
-        from models import registerNewUser
-        if(registerNewUser(name,surname,email,password)):
-           return redirect(url_for('routes.loginPage'))
+            from models import registerNewUser
+            if(registerNewUser(name,surname,email,password)):
+               return redirect(url_for('routes.loginPage'))
+            else:
+                return render_template('registerPage.html', error="User with given email already exists!")
         else:
-            return render_template('registerPage.html', error="User with given email already exists!")
+            return redirect(url_for('routes.registerPage'))
     else:
-        return redirect(url_for('routes.registerPage'))
+        return redirect(url_for('routes.mainPage'))
 
 @routes.route('/checkCredentials', methods=['POST', 'GET'])
 def checkCredentials():
-    if request.method == 'POST':
-        email = request.form['email']
-        password = request.form['password']
+    if session.get('email') == None:
+        if request.method == 'POST':
+            email = request.form['email']
+            password = request.form['password']
 
-        import re
+            import re
 
-        if not re.match(r"[^@]+@[^@]+\.[^@]+", email):
-            return render_template('loginPage.html', error="Given email doesnt contain required elements!")
-        else:
-            from models import checkCredentials
-            if(checkCredentials(email, password)):
-                return redirect(url_for('routes.mainPage'))
-            else:
+            if not re.match(r"[^@]+@[^@]+\.[^@]+", email):
                 return redirect(url_for('routes.loginPage'))
-
+            else:
+                from models import checkCredentials
+                if(checkCredentials(email, password)):
+                    session['email'] = email
+                    return redirect(url_for('routes.mainPage'))
+                else:
+                    return redirect(url_for('routes.loginPage'))
+        else:
+            return redirect(url_for('routes.loginPage'))
     else:
-        return redirect(url_for('routes.loginPage'))
+        return redirect(url_for('routes.mainPage'))
+
+@routes.route('/logout')
+def logout():
+    session.pop('email', None)
+    return redirect(url_for('routes.loginPage'))
