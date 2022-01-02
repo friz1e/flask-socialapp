0ec781a628d6fa25b3ab403ed6aafaf49a341740
kjcdev <kjciastek@gmail.com>
2022-01-02 11:29:41 +0100

    Declared addFriend and sendFriendsRequest methods, __pycache__ commit

--- models.py
+++ models.py
@@ -3,7 +3,14 @@ from passlib.hash import sha256_crypt
 
 friends = db.Table('friends',
                 db.Column('user1_id', db.Integer, db.ForeignKey('users.id')),
-                db.Column('user2_id', db.Integer, db.ForeignKey('users.id'))
+                db.Column('user2_id', db.Integer, db.ForeignKey('users.id')),
+                db.Column('created_at', db.TIMESTAMP, server_default=db.func.now(), nullable=False)
+)
+
+friends_requests = db.Table('friends_requests',
+                db.Column('user1_id', db.Integer, db.ForeignKey('users.id')),
+                db.Column('user2_id', db.Integer, db.ForeignKey('users.id')),
+                db.Column('created_at', db.TIMESTAMP, server_default=db.func.now(), nullable=False)
 )
 
 class Users(db.Model):
@@ -15,11 +22,16 @@ class Users(db.Model):
     email = db.Column(db.String, nullable=False)
     password = db.Column(db.String, nullable=False)
     created_at = db.Column(db.TIMESTAMP, server_default=db.func.now(), nullable=False)
+    friends_requests = db.relationship('Users',
+                             secondary=friends_requests,
+                             primaryjoin=(friends_requests.c.user1_id == id),
+                             secondaryjoin=(friends_requests.c.user2_id == id),
+                             lazy='dynamic')
     friends = db.relationship('Users',
                               secondary=friends,
-                              primaryjoin = (friends.c.user1_id == id),
-                              secondaryjoin = (friends.c.user2_id == id),
-                              lazy='dynamic',)
+                              primaryjoin=(friends.c.user1_id == id),
+                              secondaryjoin=(friends.c.user2_id == id),
+                              lazy='dynamic')
     posts = db.relationship('Posts', backref="user")
 
     def __init__(self, name, surname, email, password):
@@ -63,9 +75,26 @@ def addPost(email, content):
     db.session.add(newPost)
     db.session.commit()
 
+def sendFriendsRequest(email, id):
+    loggedUser = Users.query.filter_by(email=email).first()
+    userRequested = Users.query.get(id)
+
+    loggedUser.friends_requests.append(userRequested)
+    userRequested.friends_requests.append(loggedUser)
+
+    db.session.commit()
+
+#def acceptFriendsRequest(email, id):
+
+
+#def declineFriendsRequest(email, id):
+
+
 def addFriend(email, id):
     loggedUser = Users.query.filter_by(email = email).first()
-    userToBeAdded = Users.query.get(id).first()
+    userToBeAdded = Users.query.get(id)
 
     loggedUser.friends.append(userToBeAdded)
     userToBeAdded.friends.append(loggedUser)
+
+    db.session.commit()
